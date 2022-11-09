ce4c8c665021bfc0be9a397fc5b0120d9b852194
W66LIW <116950730+W66LIW@users.noreply.github.com>
2022-11-09 14:20:13 +0300

    Add files via upload

---
+++ function arrToStr/index.js
@@ -0,0 +1,20 @@
+"use strict"
+const apple = ['a','pp', 'l', 'e']
+function arrToStr1 (arr) {
+    const str = arr.join("");
+    return str;
+}
+
+function arrToStr2 (arr) {
+    let str = "";
+    for(let i = 0; i < arr.length; i++){
+        str += arr[i];
+    }
+
+    return str;
+}
+
+document.writeln(arrToStr1(apple));
+document.writeln(typeof(arrToStr1(apple)));
+document.writeln(arrToStr2(apple));
+document.writeln(typeof(arrToStr2(apple)));
