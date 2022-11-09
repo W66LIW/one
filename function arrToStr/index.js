e16d5507b86ad247cd784d54c1ec066f314605d4
W66LIW <116950730+W66LIW@users.noreply.github.com>
2022-11-09 15:18:13 +0300

    Update index.js

--- function arrToStr/index.js
+++ function arrToStr/index.js
@@ -1,22 +1,27 @@
 "use strict"
 
-
 const apple = ['a','pp', 'l', 'e']
 
 function arrToStr1 (arr) {
+    
     const str = arr.join("");
     return str;
 }
 
 function arrToStr2 (arr) {
+
     let str = "";
+
     for(let i = 0; i < arr.length; i++){
         str += arr[i];
     }
+
     return str;
 }
 
-document.writeln(arrToStr1(apple));
-document.writeln(typeof(arrToStr1(apple)));
-document.writeln(arrToStr2(apple));
-document.writeln(typeof(arrToStr2(apple)));
+document.writeln(
+    `<p>${arrToStr1(apple)} - ${typeof(arrToStr1(apple))}<br>
+        ${arrToStr2(apple)} - ${typeof(arrToStr2(apple))}</p>`);
+
+
+
