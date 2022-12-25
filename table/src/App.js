92d669a109757479dd034de13b34426ca670a438
W66LIW <etoochenmilo@gmail.com>
2022-12-25 22:39:21 +0300

    в работе

--- table/src/App.js
+++ table/src/App.js
@@ -53,13 +53,10 @@ function App() {
   }
 
   const handleAddMonthClick = (e) => {
-   // e.preventDefault();
-    let added = "added";
-    let addedd = "addedd";
+    // e.preventDefault();
     setPersons(persons.map((person) => {
-      person.added = added;
-      person.addedd = addedd;
-      return(person);
+      person.added = "added";
+      return (person);
     }))
 
 
@@ -72,62 +69,85 @@ function App() {
     }
   }
 
-  console.log(editablePersonData);
+
   console.log("persons", persons);
 
   return (
-    <div className="App-header" tabIndex={0} onKeyDown={handleKeyDown}>
+    <div>
+      <div tabIndex={0} onKeyDown={handleKeyDown}>
 
-      <div className="Add-month"><button onClick={handleAddMonthClick}>add month</button></div>
+        <div className="Add-month"><button onClick={handleAddMonthClick}>add month</button></div>
+
+        <div className="App-header">
+          <table>
+            <tbody>
+              <tr><td>Арендодатель</td><td>Договор</td><td>Арендная плата</td></tr>
+              <tr>
+                <th>
+                  <form>
+                    <input type="text" placeholder="..." onChange={(e) => setPersonData((prevState) => ({
+                      ...prevState,
+                      name: e.target.value
+                    }))}
+                      value={personData.name} />
+                  </form>
+                </th>
+                <th>
+                  <form>
+                    <input type="text" placeholder='...' onChange={(e) => setPersonData((prevState) => ({
+                      ...prevState,
+                      contract: e.target.value
+                    }))}
+                      value={personData.contract} />
+                  </form>
+                </th>
+                <th>
+                  <form>
+                    <input placeholder='...' onChange={(e) => setPersonData((prevState) => ({
+                      ...prevState,
+                      price: +e.target.value
+                    }))}
+                      value={personData.price} />
+                  </form>
+                </th>
+                <th><form onSubmit={handleSubmitPerson}><button type='submit'>save</button></form></th>
+                <th><form onClick={handleRemoveClick}><button>remove</button></form></th>
+              </tr>
+
+    
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+
+
+
 
       <div className="Table">
         <table>
           <tbody>
-            <tr><td>Арендодатель</td><td>Договор</td><td>Арендная плата</td></tr>
-            <tr>
-              <th>
-                <form>
-                  <input type="text" placeholder="..." onChange={(e) => setPersonData((prevState) => ({
-                    ...prevState,
-                    name: e.target.value
-                  }))}
-                    value={personData.name} />
-                </form>
-              </th>
-              <th>
-                <form>
-                  <input type="text" placeholder='...' onChange={(e) => setPersonData((prevState) => ({
-                    ...prevState,
-                    contract: e.target.value
-                  }))}
-                    value={personData.contract} />
-                </form>
-              </th>
-              <th>
-                <form>
-                  <input placeholder='...' onChange={(e) => setPersonData((prevState) => ({
-                    ...prevState,
-                    price: +e.target.value
-                  }))}
-                    value={personData.price} />
-                </form>
-              </th>
-              <th><form onSubmit={handleSubmitPerson}><button type='submit'>save</button></form></th>
-              <th><form onClick={handleRemoveClick}><button>remove</button></form></th>
-            </tr>
 
             {
               persons.map((person, index) => {
                 return (
-                  <tr id="person" key={persons.indexOf(person)} onDoubleClick={(e) => handleClickPerson(e, person, index)}><td>{person.name}</td><td>{person.contract}</td><td>{person.price}</td>
-                    {/* <td>{(person.price*13/87).toFixed(0)}</td> */}
-                  </tr>
-                )
+                  <tr id="person" key={persons.indexOf(person)} onDoubleClick={(e) => handleClickPerson(e, person, index)}>
+                    {Object.values(person).map((term) => {
+                      return (
+                        <td>{term}</td>
+                      )
+                    })
+                    }
+
+                  </tr>)
               })
             }
+
           </tbody>
         </table>
       </div>
+
     </div>
   );
 }
